f75e045ea71e94715a22a63369dccf8da9868955
K4ne <nick@getmighty.net>
2022-04-01 21:33:44 -0500

    Made sure 1st prompt repeats when invalid

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-let numberOfChars = prompt("Please enter your name", "0");
 var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var generateBtn = document.querySelector("#generate");
 let i = 0;
+let numberOfChars = 0;
 
+function questionone() {
+  numberOfChars = prompt("Please enter # of chars 8-128", "0");
+  if (numberOfChars > 128 || numberOfChars < 8) {
+    questionone();
+  }
+  
+}
+
+questionone();
 
 // Write password to the #password input
 function writePassword() {
-
-
   passwordText.value = password;
-
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
@@ -20,13 +26,9 @@ function generatePassword() {
   for (var i = 0 ;i <numberOfChars; i++) {
     console.log("test")
     let randomDecimal = Math.random()*chars.length;
-  let randomNumber = Math.floor(randomDecimal)
-  passwordField += chars[randomNumber].toString();
-  
+    let randomNumber = Math.floor(randomDecimal)
+    passwordField += chars[randomNumber].toString();
   }
   console.log(passwordField);
   document.querySelector("#password").innerHTML = passwordField;
-
-
-
 }
